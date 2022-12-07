1cf318d84d1ae2ba37f4f7f7ca1a048b17088bef
karankharole <karankharole2000@gmail.com>
2022-12-07 19:29:51 +0530

    flip a coin and find probability of heads and tails

---
+++ Day5problems.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5logicalprograms
+{
+    internal class Day5problems
+    {
+        public void flipCoin()
+        {
+            int Heads = 0;
+            int Tails = 0;
+            int countFlipCoin = 20;
+            Random random = new Random();
+            for (int i = 0; i < countFlipCoin; i++)
+            {
+                int coinTossCheck = random.Next(0, 2);
+                if (coinTossCheck / 2.0f < 0.5)
+                {
+                    Tails++;
+                }
+                else
+                {
+                    Heads++;
+                }
+            }
+            int heads_Percentage = (Heads * 100) / countFlipCoin;
+            Console.WriteLine("Heads percentage is : " + heads_Percentage);
+            int tails_Percentage = (Tails * 100) / countFlipCoin;
+            Console.WriteLine("Tails percentage is : " + tails_Percentage);
+        }
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Day5logicalprograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day5problems num = new Day5problems();
+               num.flipCoin();
+            
+        }
+    }
+}
